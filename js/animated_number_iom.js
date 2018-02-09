edb1271bae6596aab0605d62ef7fb61b383bb36a
DTM-Libya <35336874+DTM-Libya@users.noreply.github.com>
2018-02-09 11:39:16 +0100

    565

--- js/animated_number_iom.js
+++ js/animated_number_iom.js
@@ -17,4 +17,65 @@
 		numberStep: comma_separator_number_step
 		},
 		10000
-		)
+		);
+		
+		
+//bar chart origins 
+
+//colunn chart, Type of incidents
+
+$('#origins_mg').highcharts({
+	
+	credits: {enabled: false},
+      chart: {
+        type: 'bar',
+	 backgroundColor:'transparent' ,
+    },
+    title: {
+        text: 'Reported Shelter Settings'
+    },
+    subtitle: {
+        text: '* '
+    },
+    xAxis: {
+        categories: ['Informal settings', 'Schools', 'Host communities', 'Unfinished buildings', "Squatting on other people's properties ",'Renting ( paid by employer ) ','Renting ( self-pay ) '],
+        title: {
+            text: null
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'No. of people ',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    tooltip: {
+        valueSuffix: ' '
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'No. of People',
+		
+        data: [851, 630, 546, 171, 125, 70,60]
+    }]
+});
+
+		
+		
+		
